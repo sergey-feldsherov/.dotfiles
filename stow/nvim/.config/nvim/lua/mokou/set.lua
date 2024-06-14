f159bb33425da7a2770423d38cc5f54847231aa8
sergey-feldsherov <imnosuperman74@gmail.com>
2024-06-14 21:28:54 +0300

    Set options for indentation to use 4 spaces

--- stow/nvim/.config/nvim/lua/mokou/set.lua
+++ stow/nvim/.config/nvim/lua/mokou/set.lua
@@ -7,3 +7,9 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
+
